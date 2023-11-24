f5f340755522dc8e9206229b720287c6b1b3128c
Piotr Kulbaka <petr.kulbaka@banuba.com>
2023-11-24 11:49:45 +0300

    1.9.1-83-gf89cbbd807

---
+++ Package.swift
@@ -0,0 +1,56 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let bnbPackageVersion: Version = "1.9.1-83-gf89cbbd807"
+
+let package = Package(
+    name: "BNBFaceMatch",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "BNBFaceMatch",
+            targets: [
+                "BNBFaceMatch",
+                "BNBFaceMatch_BNBSdkCore",
+                "BNBFaceMatch_BNBFaceTracker"
+            ]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/PiotrBanuba/BNBSdkCore.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/PiotrBanuba/BNBFaceTracker.git",
+            .exact(bnbPackageVersion)
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "BNBFaceMatch",
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.9.1-83-gf89cbbd807/BNBFaceMatch.zip",
+            checksum: "1b6405e6205cf265adde44225fe808715519a3095e3b26a1ffe44e2022b472f1"
+        ),
+        .target(
+            name: "BNBFaceMatch_BNBSdkCore",
+            dependencies: [
+                .product(
+                    name: "BNBSdkCore",
+                    package: "BNBSdkCore"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBFaceMatch_BNBFaceTracker",
+            dependencies: [
+                .product(
+                    name: "BNBFaceTracker",
+                    package: "BNBFaceTracker"
+                ),
+            ]
+        ),
+    ]
+)
